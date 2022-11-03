e5b5d794141e480f15ad946325c134d555e6d2df
Gleb Akishin <ekod87@yandex.ru>
2022-11-03 12:27:41 +0300

    hw_1 done

---
+++ hw_1/target/debug/deps/hw_1-c30e6a32c004203d.d
@@ -0,0 +1,5 @@
+C:\Users\Ekod\Documents\prog\rust\otus-rust\hw_1\target\debug\deps\hw_1-c30e6a32c004203d.rmeta: src\main.rs
+
+C:\Users\Ekod\Documents\prog\rust\otus-rust\hw_1\target\debug\deps\hw_1-c30e6a32c004203d.d: src\main.rs
+
+src\main.rs:
